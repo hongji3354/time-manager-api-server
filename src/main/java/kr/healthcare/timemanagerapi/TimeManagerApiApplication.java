6bdbcaf4e4f25f5eb5fbd8e871ad64f449908533
Ji Hun Hong <hongji3354@gmail.com>
2019-12-28 15:03:36 +0900

    프로젝트 최초 생성

---
+++ src/main/java/kr/healthcare/timemanagerapi/TimeManagerApiApplication.java
@@ -0,0 +1,13 @@
+package kr.healthcare.timemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/healthcare/timemanagerapi/TimeManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.healthcare.timemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
