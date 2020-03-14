fc2bc638ebf5678e896ec61722b52c7b652a55fe
JiHun Hong <hongji3354@gmail.com>
2020-03-15 00:48:29 +0900

    설정관련 클래스는 모두 config 패키지로 이동

--- src/main/java/kr/healthcare/timemanagerapi/config/SwaggerConfig.java
+++ src/main/java/kr/healthcare/timemanagerapi/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package kr.healthcare.timemanagerapi.config.swagger;
+package kr.healthcare.timemanagerapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
