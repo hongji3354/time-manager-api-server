0a9dccfec91a31cbfee758d67f83b727f7717c32
JiHun Hong <hongji3354@gmail.com>
2020-03-14 23:57:31 +0900

    Mybatis 코드 삭제  - Mybatis에서 JPA로 변경함에 따라 Mybatis 관련 코드 삭제

--- src/main/java/kr/healthcare/timemanagerapi/TimeManagerApiApplication.java
+++ src/main/java/kr/healthcare/timemanagerapi/TimeManagerApiApplication.java
@@ -1,11 +1,9 @@
 package kr.healthcare.timemanagerapi;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "kr.healthcare.timemanagerapi")
 public class TimeManagerApiApplication {
 
     public static void main(String[] args) {
