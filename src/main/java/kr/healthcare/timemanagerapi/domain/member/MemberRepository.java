f72e6428de697ebbdb1a1ae87b54310f1745b12d
JiHun Hong <hongji3354@gmail.com>
2020-03-15 23:49:04 +0900

    MemberRepository에 입학번호/아이디 존재 여부를 확인하기 위한 메소드 추가

--- src/main/java/kr/healthcare/timemanagerapi/domain/member/MemberRepository.java
+++ src/main/java/kr/healthcare/timemanagerapi/domain/member/MemberRepository.java
@@ -3,4 +3,7 @@ package kr.healthcare.timemanagerapi.domain.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,String> {
+
+    int countByAdmissionNumber(String admissionNumber);
+    int countByMemberId(String memberId);
 }
