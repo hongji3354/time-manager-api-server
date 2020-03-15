20931d244209bfe0671bc1aefb3d9786e6d36952
JiHun Hong <hongji3354@gmail.com>
2020-03-15 23:50:36 +0900

    Member 테스트 코드 추가
     - 등록시간/수정시간 저장 테스트
     - 입학번호 중복 테스크
     - 아이디 중복 테스트

--- src/test/java/kr/healthcare/timemanagerapi/member/MemberTest.java
+++ src/test/java/kr/healthcare/timemanagerapi/member/MemberTest.java
@@ -8,17 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-
-
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-public class MemberRegisterTest {
+public class MemberTest {
 
     @Autowired
     MemberRepository memberRepository;
@@ -42,10 +42,36 @@ public class MemberRegisterTest {
         assertThat(member.getMemberId(),is("wlgnsdldp"));
         assertThat(member.getMemberPassword(),is("1234"));
         assertThat(member.getMemberGender(),is("M"));
-
-
-
-
-
     }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        LocalDateTime now  = LocalDateTime.now();
+
+        memberRepository.save(Member.builder()
+                .admissionNumber("20100444")
+                .memberName("홍지훈")
+                .memberId("wlgnsdldp")
+                .memberPassword("1234")
+                .memberGender("M")
+                .auth("u")
+                .build());
+
+        List<Member> memberList = memberRepository.findAll();
+
+        Member member = memberList.get(0);
+        assertTrue(member.getCreatedDate().isAfter(now));
+        assertTrue(member.getModifiedDate().isAfter(now));
+    }
+
+    @Test
+    public void 입학번호_중복_테스트(){
+        assertThat(memberRepository.countByAdmissionNumber("20100444"),is(1));
+    }
+
+    @Test
+    public void 아이디_중복_테스트(){
+        assertThat(memberRepository.countByMemberId("wlgnsdldp"),is(1));
+    }
+
 }
