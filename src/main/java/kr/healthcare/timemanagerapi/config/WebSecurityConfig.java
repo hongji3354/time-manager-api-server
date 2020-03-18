4fbd8a1f576201f2332496f5fff8b51d37f65ad6
JiHun Hong <hongji3354@gmail.com>
2020-03-18 23:06:52 +0900

    비밀번호를 암호화 하기 위한 BcryptPasswordEncoder 클래스 bean으로 등록

--- src/main/java/kr/healthcare/timemanagerapi/config/WebSecurityConfig.java
+++ src/main/java/kr/healthcare/timemanagerapi/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package kr.healthcare.timemanagerapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +15,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
         http.csrf().disable();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
