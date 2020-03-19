46051e843f5611e8383ac131c2727240299c02de
JiHun Hong <hongji3354@gmail.com>
2020-03-19 21:37:12 +0900

    JWT 관련 Util 구현
     - JWT 생성
     - JWT Body 가져오기

---
+++ src/main/java/kr/healthcare/timemanagerapi/util/JwtTokenUtil.java
@@ -0,0 +1,29 @@
+package kr.healthcare.timemanagerapi.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import java.util.Map;
+
+public class JwtTokenUtil {
+
+    private String secretKey = "healthcare";
+
+    //JWT 생성
+    public String deGenerateToken(Map<String, Object> claim){
+        return Jwts.builder()
+                    .setClaims(claim)
+                    .setIssuer("unist_healthcare")
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .signWith(SignatureAlgorithm.HS512,secretKey)
+                    .compact();
+    }
+
+    //JWT에서 Claim 파싱
+    public Claims getAllClaimsFromToken(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+
+
+}
