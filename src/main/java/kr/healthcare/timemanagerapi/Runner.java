209c57e46fa3086966307e13735fbeec2624ce54
JiHun Hong <hongji3354@gmail.com>
2020-03-20 22:52:18 +0900

    Runner 내용 삭제

--- src/main/java/kr/healthcare/timemanagerapi/Runner.java
+++ src/main/java/kr/healthcare/timemanagerapi/Runner.java
@@ -1,28 +1,21 @@
 package kr.healthcare.timemanagerapi;
 
-import kr.healthcare.timemanagerapi.jwt.JWT;
-import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.Jwts;
+import kr.healthcare.timemanagerapi.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class Runner implements ApplicationRunner {
 
-    @Value("${driverClassName}")
-    String driverClassName;
-    @Value("${jdbcUrl}")
-    String jdbcUrl;
-    @Value("${userName}")
-    String userName;
-    @Value("${password}")
-    String password;
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(driverClassName);
-        System.out.println(jdbcUrl);
-        System.out.println(userName);
-        System.out.println(password);
+
     }
 }
