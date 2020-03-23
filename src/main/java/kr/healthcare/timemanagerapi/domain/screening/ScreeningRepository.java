0dcac576b74bd1a26b650d8a65273845a022ac5c
ji hun hong <hongji3354@gmail.com>
2020-03-23 17:03:11 +0900

    스크리닝 데이터 Entity 및 Repository 생성

---
+++ src/main/java/kr/healthcare/timemanagerapi/domain/screening/ScreeningRepository.java
@@ -0,0 +1,6 @@
+package kr.healthcare.timemanagerapi.domain.screening;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreeningRepository extends JpaRepository<Screening, Long> {
+}
