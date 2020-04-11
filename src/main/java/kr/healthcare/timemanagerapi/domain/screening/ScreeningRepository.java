1d55ec499e3ed2f81981487abea2e6233a333258
JiHun Hong <hongji3354@gmail.com>
2020-04-11 21:40:39 +0900

    기획이 변경되어 스크리닝 domain 삭제

--- src/main/java/kr/healthcare/timemanagerapi/domain/screening/ScreeningRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.healthcare.timemanagerapi.domain.screening;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScreeningRepository extends JpaRepository<Screening, Long> {
-}
