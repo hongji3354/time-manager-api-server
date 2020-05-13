24a9c7ac725781d35ad69750f7d8feca038cd01d
Ji Hun Hong <hongji3354@gmail.com>
2020-05-13 17:17:34 +0900

    Response resultCode도 상수에 추가

--- src/main/java/kr/healthcare/timemanagerapi/constant/ResponseFailMessage.java
+++ src/main/java/kr/healthcare/timemanagerapi/constant/ResponseFailMessage.java
@@ -7,9 +7,12 @@ import lombok.Getter;
 @Getter
 public enum  ResponseFailMessage {
 
+    FAIL("FAIL"),
+    SUCCESS("SUCCESS"),
     H000001("이미 가입된 학번입니다."),
     H000002("이미 사용된 이메일 입니다."),
-    H000003("이미 가입된 학번이거나 이미 사용되고 있는 이메일 입니다.");
+    H000003("이미 가입된 학번이거나 이미 사용되고 있는 이메일 입니다."),
+    H000004("아이디 또는 비밀번호를 확인해주세요.");
 
     private String message;
 
