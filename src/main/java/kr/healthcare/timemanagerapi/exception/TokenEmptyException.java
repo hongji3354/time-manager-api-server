a73d6563be5236478e131fb1382f8248b14d5442
JiHun Hong <hongji3354@gmail.com>
2020-05-14 22:52:53 +0900

    Custom Exception 추가
    Token이 비어있을 때 발생하는 Exception
    권한이 없는 API에 접근시 발생하는 Exception

---
+++ src/main/java/kr/healthcare/timemanagerapi/exception/TokenEmptyException.java
@@ -0,0 +1,8 @@
+package kr.healthcare.timemanagerapi.exception;
+
+public class TokenEmptyException extends Exception {
+
+    public TokenEmptyException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/kr/healthcare/timemanagerapi/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package kr.healthcare.timemanagerapi.exception;
+
+public class UnauthorizedException extends Exception {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
