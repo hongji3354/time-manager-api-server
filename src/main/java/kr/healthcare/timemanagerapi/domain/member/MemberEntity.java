096ca6ed4a00c805ff7639a2c66589543c51c373
Ji Hun Hong <hongji3354@gmail.com>
2020-05-15 17:59:26 +0900

    상담관리 Entity에서 회원 Entity의 email을 참조하는데 PK가 아닌 Column을 참조하니 오류가 발생해 Serializable 상속 받음

--- src/main/java/kr/healthcare/timemanagerapi/domain/member/MemberEntity.java
+++ src/main/java/kr/healthcare/timemanagerapi/domain/member/MemberEntity.java
@@ -5,19 +5,18 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.DynamicInsert;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "MEMBER_TBL")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class MemberEntity extends BaseTimeEntity {
+//ConsultingManageEntity에서 MemberEntity의 Email을 참조하는데 PK가 아닌 컬럼을 참조해서 그런지 오류가 발생함. 해결방법은 Serializable 상속받는 것
+// https://www.inflearn.com/questions/16570 참고함!
+public class MemberEntity extends BaseTimeEntity implements Serializable {
 
     @Id
     @Column(length = 8)
